592ac0c1dfceabf3f0269a50c51334c4948d9534
Hyunjun Yoon <yhj940719@gmail.com>
2021-06-06 23:32:39 -0400

    passport-jwt added

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/(^\S)([^\W])*$/, { message: 'Please enter valid username' })
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least 1 Uppercase, 1 Lowercase and 1 number or special character',
+  })
+  password: string;
+}
